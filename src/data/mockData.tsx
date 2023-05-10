f2edfc0f9da8b48d7fcd042f4ffc1dbe71ce2e87
Mashaal Ahmed <pepsparks@gmail.com>
2023-05-10 18:10:04 +1000

    Initial commit

---
+++ src/data/mockData.tsx
@@ -0,0 +1,37 @@
+export const mockSeifa = [
+    {
+        "localGovtAreas": "New South Wales",
+        "locations": "Albury (C)",
+        "relativeDisadvantage": 979,
+        "relativeAdvantage": 967,
+        "seifaId": 1
+      },
+      {
+        "localGovtAreas": "New South Wales",
+        "locations": "Armidale Dumaresq (A)",
+        "relativeDisadvantage": 987,
+        "relativeAdvantage": 985,
+        "seifaId": 2
+      },
+      {
+        "localGovtAreas": "New South Wales",
+        "locations": "Ashfield (A)",
+        "relativeDisadvantage": 1015,
+        "relativeAdvantage": 1031,
+        "seifaId": 3
+      },
+      {
+        "localGovtAreas": "New South Wales",
+        "locations": "Auburn (C)",
+        "relativeDisadvantage": 917,
+        "relativeAdvantage": 944,
+        "seifaId": 4
+      },
+      {
+        "localGovtAreas": "New South Wales",
+        "locations": "Ballina (A)",
+        "relativeDisadvantage": 989,
+        "relativeAdvantage": 980,
+        "seifaId": 5
+      }
+]
